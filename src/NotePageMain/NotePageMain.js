8127976ebdd7f00bf21064b1ed97a063e786e76d
sunnyAli <57465011+sunnyaliiii@users.noreply.github.com>
2020-04-20 00:56:07 -0700

    Update NotePageMain.js

--- src/NotePageMain/NotePageMain.js
+++ src/NotePageMain/NotePageMain.js
@@ -1,39 +1,42 @@
-import React from 'react'
+import React, {Component } from 'react'
 import Note from '../Note/Note'
-import ApiContext from '../ApiContext'
-import { findNote } from '../notes-helpers'
-import './NotePageMain.css'
+import { findNote } from '../notes-helpers';
+import './NotePageMain.css';
+// import { shape, string } from 'prop-types'
+import NoteContext from '../NoteContext';
 
-export default class NotePageMain extends React.Component {
-  static defaultProps = {
-    match: {
-      params: {}
-    }
-  }
-  static contextType = ApiContext
-
-  handleDeleteNote = noteId => {
-    this.props.history.push(`/`)
-  }
-
+export default class NotePageMain extends Component{
+  static contextType = NoteContext;
   render() {
-    const { notes=[] } = this.context
-    const { noteId } = this.props.match.params
-    const note = findNote(notes, noteId) || { content: '' }
-    return (
+    const { noteId } = this.props.match.params;
+    const note = findNote(this.context.notes, parseInt(noteId));
+  return (
       <section className='NotePageMain'>
         <Note
           id={note.id}
           name={note.name}
           modified={note.modified}
-          onDeleteNote={this.handleDeleteNote}
         />
         <div className='NotePageMain__content'>
           {note.content.split(/\n \r|\n/).map((para, i) =>
             <p key={i}>{para}</p>
           )}
         </div>
-      </section>
+      </section>        
     )
   }
 }
+
+NotePageMain.defaultProps = {
+  note: {
+    content: '',
+  }
+}
+
+// NotePageMain.propTypes = {
+//   match: shape({
+//     params: shape({
+//       noteId: string
+//     })
+//   })
+// }
